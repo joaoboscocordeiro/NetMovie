34c0961d006db084a8fb6dae38b249c36d5ea556
João Bosco <ti.junior@gmail.com>
2024-01-04 20:27:28 -0300

    Criação da tela de Forgot

---
+++ app/src/main/java/com/jbc/appnetmovie/presenter/auth/forgot/ForgotFragment.kt
@@ -0,0 +1,10 @@
+package com.jbc.appnetmovie.presenter.auth.forgot
+
+import androidx.fragment.app.Fragment
+import com.jbc.appnetmovie.R
+
+/*
+ * Created by Joao Bosco on 04/01/2024.
+ */
+class ForgotFragment : Fragment(R.layout.fragment_forgot) {
+}
