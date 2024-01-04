a9270e0dfe5ce7c2955d6c38603d01f63590d7c8
João Bosco <ti.junior@gmail.com>
2024-01-04 19:24:53 -0300

    Criação da tela de Login

---
+++ app/src/main/java/com/jbc/appnetmovie/presenter/auth/login/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.jbc.appnetmovie.presenter.auth.login
+
+import androidx.fragment.app.Fragment
+import com.jbc.appnetmovie.R
+
+/*
+ * Created by Joao Bosco on 03/01/2024.
+ */
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
