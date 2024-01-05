26aa313f6332f5c30a400badf9d86872494529d3
João Bosco <ti.junior@gmail.com>
2024-01-05 08:47:09 -0300

    Configurando login no Firebase

--- app/src/main/java/com/jbc/appnetmovie/presenter/auth/login/LoginFragment.kt
+++ app/src/main/java/com/jbc/appnetmovie/presenter/auth/login/LoginFragment.kt
@@ -1,9 +1,15 @@
 package com.jbc.appnetmovie.presenter.auth.login
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.jbc.appnetmovie.R
 import com.jbc.appnetmovie.databinding.FragmentLoginBinding
 import com.jbc.appnetmovie.util.BaseFragment
+import com.jbc.appnetmovie.util.StateView
 import dagger.hilt.android.AndroidEntryPoint
 
 /*
@@ -16,4 +22,64 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
 ) {
 
     private val viewModel: LoginViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        binding?.btnLoginSignIn?.setOnClickListener { validData() }
+
+        binding?.progress?.let {
+            Glide
+                .with(requireContext())
+                .load(R.drawable.loading)
+                .into(it)
+        }
+    }
+
+    private fun validData() {
+        val email = binding?.editLoginEmail?.text.toString().trim()
+        val password = binding?.editLoginPassword?.text.toString().trim()
+
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+
+                login(email, password)
+
+            } else {
+                binding?.editLoginPassword?.requestFocus()
+            }
+        } else {
+            binding?.editLoginEmail?.requestFocus()
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        viewModel.login(email, password).observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Error -> {
+                    binding?.progress?.isVisible = true
+                }
+                is StateView.Loading -> {
+                    binding?.progress?.isVisible = false
+                    Toast.makeText(
+                        requireContext(),
+                        "Usuário logado com sucesso!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is StateView.Success -> {
+                    binding?.progress?.isVisible = false
+                    Toast.makeText(
+                        requireContext(),
+                        "ERRO ao logar usuário!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
 }
